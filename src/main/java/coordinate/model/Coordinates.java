ca1134bdd874124a6cc79d94001fd28dcd4f8082
WKingDev <wkingdevloper@gmail.com>
2022-10-27 13:44:01 +0900

    feat-new : 사각형의 경우 직사각형 or 정사각형의 모양만 허용하도록 한다.

--- src/main/java/coordinate/model/Coordinates.java
+++ src/main/java/coordinate/model/Coordinates.java
@@ -1,6 +1,7 @@
 package coordinate.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Coordinates {
@@ -18,6 +19,7 @@ public class Coordinates {
             this.coordinateList.add(coordinate);
         }
         setFigure();
+        validRectangle();
         return this.coordinateList;
     }
 
@@ -33,6 +35,20 @@ public class Coordinates {
         this.figure = Figure.LINE;
         if (this.coordinateList.size() == 4) {
             this.figure = Figure.SQUARE;
+        }
+    }
+
+    private void validRectangle() {
+        if (this.figure == Figure.SQUARE) {
+            HashSet<Integer> list = new HashSet<>();
+            this.coordinateList.stream().forEach(coordinate ->{
+                    list.add(coordinate.getPositionX());
+                    list.add(coordinate.getPositionY());
+
+            });
+            if (list.size() != 3) {
+                throw new RuntimeException("직사각형이 아닙니다.");
+            }
         }
     }
 }
